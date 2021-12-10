ef89cb224cf45a8019e71ad75e3c01797a5c0884
JosueHuashuayo <jhuashuayo@unsa.edu.pe>
2021-12-10 18:15:49 -0500

    comprobacion del nombre del bot , inicio de primera funcion

--- asistenteVirtual.py
+++ asistenteVirtual.py
@@ -2,16 +2,28 @@ import speech_recognition as sr
 import pyttsx3
 
 listener = sr.Recognizer()
-
+nombre = "alexa"
 siri = pyttsx3.init()
+siri.setProperty('rate',125)
 
-try:
-    with sr.Microphone() as source :
-        print("Escuchando ...")
-        voice = listener.listen(source)
-        rec = listener.recognize_google(voice)
-        print(rec)
-        siri.say(rec)
-        siri.runAndWait()
-except:
-    pass
+def hablar(text):
+    siri.say(text)
+    siri.runAndWait()
+def Escuchando():
+    try:
+        with sr.Microphone() as source :
+            print("Escuchando ...")
+            voice = listener.listen(source)
+            grabacion = listener.recognize_google(voice)
+            grabacion = grabacion.lower()
+            if nombre in grabacion:
+                grabacion = grabacion.replace(nombre," ")
+                print(grabacion)
+    except:
+        pass
+    return grabacion
+def Reproducir():
+    grabacion = Escuchando()
+    if "reproduce" in grabacion:
+        print("Reproduciendo")
+Reproducir()       
