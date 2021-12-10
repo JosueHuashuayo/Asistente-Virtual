29ac16fd0c74ffae9337d7028813b0df056ae96e
JosueHuashuayo <jhuashuayo@unsa.edu.pe>
2021-12-10 17:31:01 -0500

    Usando la biblioteca pyttsx3 , bot con voz

--- asistenteVirtual.py
+++ asistenteVirtual.py
@@ -1,11 +1,17 @@
 import speech_recognition as sr
+import pyttsx3
+
 listener = sr.Recognizer()
+
+siri = pyttsx3.init()
 
 try:
     with sr.Microphone() as source :
         print("Escuchando ...")
         voice = listener.listen(source)
-        rec = listener.recognize(voice)
+        rec = listener.recognize_google(voice)
         print(rec)
+        siri.say(rec)
+        siri.runAndWait()
 except:
     pass
