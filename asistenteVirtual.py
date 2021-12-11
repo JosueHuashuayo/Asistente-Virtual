6d99ddb371bcf778036f93fc9b1c654a9e7e3a54
JosueHuashuayo <jhuashuayo@unsa.edu.pe>
2021-12-10 19:42:47 -0500

    Complete la funcion para reproducir musica en Youtube

--- asistenteVirtual.py
+++ asistenteVirtual.py
@@ -1,29 +1,35 @@
 import speech_recognition as sr
 import pyttsx3
+import pywhatkit
+
 
 listener = sr.Recognizer()
 nombre = "alexa"
-siri = pyttsx3.init()
-siri.setProperty('rate',125)
+alexa = pyttsx3.init()
+alexa.setProperty('rate',125)
 
 def hablar(text):
-    siri.say(text)
-    siri.runAndWait()
+    alexa.say(text)
+    alexa.runAndWait()
 def Escuchando():
     try:
         with sr.Microphone() as source :
             print("Escuchando ...")
             voice = listener.listen(source)
-            grabacion = listener.recognize_google(voice)
+            grabacion = listener.recognize_google(voice,language="es-ES")
             grabacion = grabacion.lower()
             if nombre in grabacion:
-                grabacion = grabacion.replace(nombre," ")
-                print(grabacion)
+                grabacion = grabacion.replace(nombre,"")
+                
     except:
         pass
     return grabacion
-def Reproducir():
+    
+def Youtube():
     grabacion = Escuchando()
     if "reproduce" in grabacion:
-        print("Reproduciendo")
-Reproducir()       
+        musica = grabacion.replace("reproduce","")
+        print(musica)
+        hablar("Reproduciendo" + musica)
+        pywhatkit.playonyt(musica)
+Youtube()
